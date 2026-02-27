15ab7ee6d4984ddbad61ccfef8168ac59d017258
Anuj Upadhyay <anuj.upadhyay10@gmail.com>
2026-02-27 12:47:36 +0530

    SCRUM-19-Pathfinding - eloq-js - bot - pathfinding done.

--- js/eloq_js/ch007_project1_robot/demos/demo_001_meadowfield.js
+++ js/eloq_js/ch007_project1_robot/demos/demo_001_meadowfield.js
@@ -110,4 +110,43 @@
 
     return { direction: memory[0], memory: memory.slice(1) }
   }
+
+  function find_route(graph, from, to) {
+    let work = [{ at: from, route: [] }]
+
+    for (let i = 0; i < work.length; i++) {
+      let {at, route} = work[i]
+
+      for (let place of graph[at]) {
+        if (place == to) return route.concat(place)
+
+        if (!work.some(w => w.at == place)) {
+          work.push({ at: place, route: route.concat(place) })
+        }
+      }
+    }
+  }
+
+  function goal_oriented_robot({ place, parcels }, route) {
+    if (route.length == 0) {
+      let parcel = parcels[0]
+
+      if (parcel.place != place) {
+        route = find_route(road_graph, place, parcel.place)
+      } else {
+        route = find_route(road_graph, place, parcel.address)
+      }
+    }
+
+    return { direction: route[0], memory: route.slice(1) }
+  }
+
+  // Test route_robot
+  console.log("=== Route Robot ===");
+  run_robot(VillageState.random(), route_robot, []);
+
+  // Test goal_oriented_robot  
+  console.log("=== Goal Oriented Robot ===");
+  run_robot(VillageState.random(), goal_oriented_robot, []);
+
 })()
