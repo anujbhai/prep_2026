b21c1c975cdb0afe1e2c1a2427f05f73df09e735
Anuj Upadhyay <anuj.upadhyay10@gmail.com>
2026-02-27 16:41:57 +0530

    SCRUM-20-TEST-Bot-measurement - eloq-js - bot - measurement done.

--- js/eloq_js/ch007_project1_robot/demos/demo_001_meadowfield.js
+++ js/eloq_js/ch007_project1_robot/demos/demo_001_meadowfield.js
@@ -56,17 +56,30 @@
   console.log(first.place)
 
   // Simulation
+  // ex. 00. Original
+  // function run_robot(state, robot, memory) {
+  //   for (let turn = 0;; turn++) {
+  //     if (state.parcels.length == 0) {
+  //       console.log(`Done in ${ turn } turns`)
+  //       break
+  //     }
+
+  //     let action = robot(state, memory)
+  //     state = state.move(action.direction)
+  //     memory = action.memory
+  //     console.log(`Moved to ${ action.direction }`)
+  //   }
+  // }
+
+  // ex. 01. bot comparision
   function run_robot(state, robot, memory) {
     for (let turn = 0;; turn++) {
       if (state.parcels.length == 0) {
-        console.log(`Done in ${ turn } turns`)
-        break
+        return turn;
       }
-
       let action = robot(state, memory)
       state = state.move(action.direction)
       memory = action.memory
-      console.log(`Moved to ${ action.direction }`)
     }
   }
 
@@ -149,4 +162,23 @@
   console.log("=== Goal Oriented Robot ===");
   run_robot(VillageState.random(), goal_oriented_robot, []);
 
+  function compare_robots(robot1, memory1, robot2, memory2) {
+    let tasks = 100
+    let robot1_total = 0, robot2_total = 0
+
+    for (let i = 0; i < tasks; i++) {
+      let state = VillageState.random()
+
+      robot1_total += run_robot(state, robot1, memory1)
+      robot2_total += run_robot(state, robot2, memory2)
+    }
+
+    let avg1 = Math.round(robot1_total / tasks)
+    let avg2 = Math.round(robot2_total / tasks)
+
+    console.log(`Bot 1: ${ avg1 } avg turns`)
+    console.log(`Bot 2: ${ avg2 } avg turns`)
+  }
+  compare_robots(route_robot, [], goal_oriented_robot, [])
+  compare_robots(goal_oriented_robot, [], route_robot, [])
 })()
