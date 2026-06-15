630633eecd6027b492fbee6887d96c9ed6647569
Anuj Upadhyay <anuj.upadhyay10@gmail.com>
2026-06-15 14:51:04 +0530

    prep-2026-ai - neuralnine-langchain-py - middlewares (dynamic prompting and dynamic model selection).

--- ai/langchain/langchain-crash-neuralnine/src/main.py
+++ ai/langchain/langchain-crash-neuralnine/src/main.py
@@ -1,61 +1,82 @@
+from dataclasses import dataclass
 from dotenv import load_dotenv
+
+from langchain_groq import ChatGroq
+from langchain.agents import create_agent
+from langchain.agents.middleware import wrap_model_call
+from langchain.messages import SystemMessage, HumanMessage, AIMessage
 
 load_dotenv()
 
-# from langchain.chat_models import init_chat_model
-from langchain_groq import ChatGroq
-from langchain.agents import create_agent
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_core.tools import create_retriever_tool
+basic_model = ChatGroq(model="llama-3.1-8b-instant")
+complex_model = ChatGroq(model="llama-3.3-70b-versatile")
 
-embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+@wrap_model_call
+def dynamic_model_selection(request, handler):
+  message_count = len(request.state["messages"])
 
-texts = [
-  "I love apples.",
-  "I enjoy oranges.",
-  "I think pears taste very good.",
-  "I hate bananas.",
-  "I dislike raspberries.",
-  "I despise mangoes",
-  "I love Linux.",
-  "I hate Windows."
-]
+  model = complex_model if message_count > 3 else basic_model
 
-vector_store = FAISS.from_texts(texts, embedding=embeddings)
-
-retriever = vector_store.as_retriever(search_kwargs={"k": 3})
-
-print(vector_store.similarity_search("what fruits does the person like?", k=3))
-
-retriever_tool=create_retriever_tool(retriever, name="kb_search", description="Search the small product/fruit knowledge base for information.")
-
-# model = init_chat_model(
-#   model="llama-3.1-8b-instant",
-#   model_provider="groq",
-# )
-
-model = ChatGroq(
-  model="llama-3.1-8b-instant",
-)
+  request = request.override(model=model)
+  
+  return handler(request)
 
 agent = create_agent(
-  model=model,
-  tools=[retriever_tool],
-  system_prompt=(
-    "You are helpful assistant. For questions about Macs, apples, or laptops, "
-    "for call the kb_search tool to retrieve context then answer succinctly. Maybe you have to use it multiple time before answering."
-  )
+  model=basic_model,
+  middleware=[dynamic_model_selection]
 )
 
-result = agent.invoke({
+response = agent.invoke({
   "messages": [
-    {
-      "role": "user",
-      "content": "What three fruits does the person like and what three fruits does the person dislike?"
-    }
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="What is 1 + 1?"),
+    HumanMessage(content="What is 1 + 1?"),
+    HumanMessage(content="What is 1 + 1?"),
+    HumanMessage(content="What is 1 + 1?"),
+    HumanMessage(content="What is 1 + 1?")
   ]
 })
 
-print(result)
-print(result["messages"][-1].content)
+print(response["messages"][-1].content)
+print(response["messages"][-1].response_metadata["model_name"])
+
+# @dataclass
+# class Context:
+#   user_role: str
+
+# @dynamic_prompt
+# def user_role_prompt(request: ModelRequest) -> str:
+#   user_role = request.runtime.context.user_role
+
+#   base_prompt = "You are a helpful and very concise assistant."
+
+#   match user_role:
+#     case "expert":
+#       return f"{base_prompt} Provide detailed technical responses."
+#     case "beginner":
+#       return f"{base_prompt} Keep your explanations simple and basic."
+#     case "child":
+#       return f"{base_prompt} Explain everything as if you were literally talking to a five year old."
+#     case _:
+#       return base_prompt
+
+# model = ChatGroq(
+#   model="llama-3.1-8b-instant"
+# )
+
+# agent = create_agent(
+#   model = basic_model,
+#   middleware=[user_role_prompt],
+#   context_schema=Context
+# )
+
+# response = agent.invoke({
+#   "messages": [
+#     {
+#       "role": "user",
+#       "content": "Explain PCA."
+#     }
+#   ]
+# }, context = Context(user_role="expert"))
+
+# print(response)
